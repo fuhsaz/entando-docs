eaae5cc1afa531187559624ac2b059e2966fbd81
jyunmitch <j.yunmitchell@entando.com>
2022-12-06 15:14:45 -0600

    deploy

---
+++ assets/js/216.5833f9ac.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[216],{1064:function(e,t,n){"use strict";n.r(t);n(24),n(54);var o={name:"Redirect",props:{to:{type:String,required:!0}},beforeMount:function(){document.location.replace(this.to)}},r=n(32),i=Object(r.a)(o,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=i.exports}}]);
